2ff8e998f41a549f7f5387f978f6d24b3a447bb5
Innosan <reau.beua@gmail.com>
2024-01-14 05:56:48 +0300

    error handling

--- services/parse_cs_market.py
+++ services/parse_cs_market.py
@@ -1,18 +1,29 @@
 import requests
 from bs4 import BeautifulSoup
+from urllib.parse import quote
 
 from models.CsMarketItem import MarketItem
+from models.Error import Error
 
-market_url = "https://market-old.csgo.com/?s=pop&t=all&sd=desc&search="
+market_search_url = "https://market-old.csgo.com/?s=pop&search="
+market_base_url = "https://market-old.csgo.com"
 
 
 def parse_market_item(item_title: str):
-    response = requests.get(market_url + item_title)
+    response = requests.get(market_search_url + quote(item_title) + "&sd=desc")
     soup = BeautifulSoup(response.content, 'html.parser')
 
     items = []
+    souped_search_items = []
 
-    souped_search_items = soup.find("div", class_="market-items").find_all("a", class_="item").copy()
+    market_items = soup.find("div", class_="market-items")
+    print(market_items)
+    print(soup)
+
+    if market_items is not None:
+        souped_search_items = market_items.find_all("a", class_="item").copy()
+    else:
+        return Error(404, "Failed to fetch item!")
 
     for souped_item in souped_search_items:
         item = MarketItem()
@@ -21,7 +32,7 @@ def parse_market_item(item_title: str):
 
         item.price = souped_item.find_next("div", class_="price").text
         item.title = souped_item.find_next("div", class_="name").text
-        item.link = souped_item["href"]
+        item.link = market_base_url + souped_item["href"]
         item.image = item_image
 
         items.append(item)
--- services/parse_steam_market.py
+++ services/parse_steam_market.py
@@ -1,5 +1,6 @@
 import requests
 
+from models.Error import Error
 from models.MarketItem import MarketItem
 
 market_url = "https://steamcommunity.com/market/search/render/?appid=730&norender=1&query="
@@ -7,12 +8,22 @@ steam_link = "https://steamcommunity.com/market/listings/730/"
 
 
 def parse_steam_item(item_title: str):
-    response = requests.get(market_url + item_title)
-    results = response.json()['results'][0]
+    try:
+        response = requests.get(market_url + item_title)
+    except:
+        return Error(400, "Steam request failure!")
+    print(response)
+
+    results = {}
+
+    if response.json()['results']:
+        results = response.json()['results'][0]
+    else:
+        return Error(404, "Steam item parse failure!")
 
     item = MarketItem()
     item.price = results["sell_price_text"]
     item.title = results["name"]
-    item.link = steam_link + item_title
+    item.link = steam_link + item.title
 
     return item.to_dict()
